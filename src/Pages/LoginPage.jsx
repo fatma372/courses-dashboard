a08e7afa54a00eca0851381be6e8096ab2a6ef4b
fatma372 <fa1640@fayoum.edu.eg>
2025-04-30 18:48:54 +0300

    edit style

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
 
   return (  
     <div className="flex items-center justify-center min-h-screen bg-gray-800">  
-      <div className="w-full max-w-lg p-8 bg-white rounded shadow">  
+      <div className="w-full max-w-lg p-8 bg-white rounded shadow mx-3">  
         <h2 className="mb-6 text-2xl font-bold text-center text-gray-700">Login</h2>  
         <form onSubmit={handleSubmit} noValidate>  
           {/* email input */}  
